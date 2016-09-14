49a5921a02d0dd8defb4a9b7f25fe477d61a1269
Alan Sikora <alansikora@gmail.com>
2016-09-14 16:06:17 -0300

    new version

--- huedollar.rb
+++ huedollar.rb
@@ -1,35 +1,22 @@
 class Huedollar < Formula
   desc "Get's dollar conversion rates on your notifications"
   homepage "git@github.com:alansikora/huedollar.git"
-  url "https://github.com/stedolan/jq/releases/download/jq-1.5/jq-1.5.tar.gz"
-  sha256 "c4d2bfec6436341113419debf479d833692cc5cdab7eb0326b5a4d4fbe9f493c"
-  revision 2
+  url "https://github.com/alansikora/huedollar/archive/0.1.tar.gz"
+  sha256 "a095ed49bba5680be8a54530ea794815b83cd7b22dda352dd79df2e119f598e3"
+  revision 1
 
   bottle do
     cellar :any
-    sha256 "f3a31965bcaf187dae9a46b8f7acf02af7d9bbadb21bd834197ed12699b63c25" => :sierra
-    sha256 "16fd34adec21188f7e13655cde69289acf0a87f4241395357f1c4d47f492eda1" => :el_capitan
-    sha256 "8fbfede40ab806d8a93c1551a00af4aa46f7289d47fbb96836c58197f33e13a5" => :yosemite
-    sha256 "b55d226db14edc9ada42ecfd00ae64497702c616247fc8d5d7c35c01d25b26e5" => :mavericks
   end
 
   head do
-    url "https://github.com/stedolan/jq.git"
-
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-    depends_on "libtool" => :build
+    url "https://github.com/alansikora/huedollar.git"
   end
 
-  depends_on "oniguruma" # jq depends > 1.5
+  depends_on "jq"
 
   def install
-    system "autoreconf", "-iv" unless build.stable?
-    system "./configure", "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--disable-maintainer-mode",
-                          "--prefix=#{prefix}"
-    system "make", "install"
+    # system "make", "install"
   end
 
   test do
--- huedollar.sh
+++ huedollar.sh
@@ -1,54 +1,96 @@
 #!/bin/bash
 
+get_variable() {
+  cat $HOME/.huedollar | grep -w $1 | cut -d'=' -f2
+}
+
+set_variable() {
+  remove_variable $1
+  echo $1=$2 >> $HOME/.huedollar
+}
+
+remove_variable() {
+  sed -i.bak "/^$1=/d" $HOME/.huedollar
+}
+
 # Check if should run
-WEEKDAY=$(date +%u)
-HOUR=$(date +%-H)
-MIN=$(date +%-M)
+WEEKDAY=`date +%u`
+HOUR=`date +%-H`
+MIN=`date +%-M`
 [ $WEEKDAY -eq 0 -o $WEEKDAY -eq 7 ] && exit
 [ $HOUR -le 8 -o $HOUR -eq 9 -a $MIN -le 0 ] && exit
 [ $HOUR -ge 18 -o $HOUR -eq 17 -a $MIN -ge 20 ] && exit
 
 DOLLAR=`curl -s 'http://api.promasters.net.br/cotacao/v1/valores?moedas=USD&alt=json' | /usr/local/bin/jq -r '.valores.USD.valor'`
 
-read LAST_DAY_DOLLAR < last_day_dollar.txt
-read OLD_DOLLAR < dollar.txt
-echo $DOLLAR > dollar.txt
+LAST_DAY_DOLLAR=`get_variable 'LAST_DAY_DOLLAR'`
+OLD_DOLLAR_A=`get_variable 'OLD_DOLLAR_A'`
+OLD_DOLLAR_B=`get_variable 'OLD_DOLLAR_B'`
+
+if [ $DOLLAR == $OLD_DOLLAR_A ]; then
+  OLD_DOLLAR=$OLD_DOLLAR_B
+else
+  OLD_DOLLAR=$OLD_DOLLAR_A
+
+  set_variable OLD_DOLLAR_B $OLD_DOLLAR_A
+  set_variable OLD_DOLLAR_A $DOLLAR
+fi
 
 if [ $HOUR \> 5 ];
 then
-    echo $DOLLAR > last_day_dollar.txt
-fi;
+    set_variable LAST_DAY_DOLLAR $DOLLAR
+fi
 
 if [ $DOLLAR \> $OLD_DOLLAR ];
 then
     SYMBOL="+"
 else
     SYMBOL="-"
-fi;
+fi
 
 if [ $DOLLAR == $OLD_DOLLAR ];
 then
     SYMBOL="="
-fi;
+fi
 
 if [ $DOLLAR \> $LAST_DAY_DOLLAR ];
 then
     LAST_DAY_SYMBOL="+"
 else
     LAST_DAY_SYMBOL="-"
-fi;
+fi
 
 if [ $DOLLAR == $LAST_DAY_DOLLAR ];
 then
     LAST_DAY_SYMBOL="="
-fi;
+fi
 
-VARIATION=`awk -v t1="$OLD_DOLLAR" -v t2="$DOLLAR" 'BEGIN{printf "%.2f", (t2-t1)/t1 * 100}'`
-LAST_DAY_VARIATION=`awk -v t1="$LAST_DAY_DOLLAR" -v t2="$DOLLAR" 'BEGIN{printf "%.2f", (t2-t1)/t1 * 100}'`
+if [ $OLD_DOLLAR \> 0 ];
+then
+  VARIATION=`awk -v t1="$OLD_DOLLAR" -v t2="$DOLLAR" 'BEGIN{printf " (%.2f%%)", (t2-t1)/t1 * 100}'`
+else
+  VARIATION=''
+fi
+
+if [ "$LAST_DAY_DOLLAR" != "0.0000" ];
+then
+  LAST_DAY_VARIATION=`awk -v t1="$LAST_DAY_DOLLAR" -v t2="$DOLLAR" 'BEGIN{printf " (%.2f%%)", (t2-t1)/t1 * 100}'`
+else
+  LAST_DAY_VARIATION=''
+fi
+
 LAST_DAY_DIFFERENCE=`bc <<< "$DOLLAR-$LAST_DAY_DOLLAR"`
 
 DOLLAR=`sed 's/\./,/' <<< $DOLLAR`
 OLD_DOLLAR=`sed 's/\./,/' <<< $OLD_DOLLAR`
 LAST_DAY_DOLLAR=`sed 's/\./,/' <<< $LAST_DAY_DOLLAR`
 
-osascript -e 'display notification "Ontem: R$ '$LAST_DAY_DOLLAR' '$LAST_DAY_SYMBOL' R$ '$LAST_DAY_DIFFERENCE' ('$LAST_DAY_VARIATION'%)" with title "Dollarific" subtitle "R$ '$DOLLAR' '$SYMBOL' R$ '$OLD_DOLLAR' ('$VARIATION'%)"'
+TITLE='Huedollar'
+SUBTITLE='R$ '$DOLLAR' '$SYMBOL' R$ '$OLD_DOLLAR''"$VARIATION"''
+if [ "$LAST_DAY_DOLLAR" != '0,0000' ]; then
+  CONTENT='Ontem: R$ '$LAST_DAY_DOLLAR' '$LAST_DAY_SYMBOL' R$ '$LAST_DAY_DIFFERENCE''"$LAST_DAY_VARIATION"''
+else
+  CONTENT='Ontem: Não disponível'
+fi
+
+osascript -e 'display notification "'"$CONTENT"'" with title "'"$TITLE"'" subtitle "'"$SUBTITLE"'"'
